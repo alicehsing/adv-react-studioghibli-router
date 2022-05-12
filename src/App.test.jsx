861014a494af0927838fb7258618988076f0a372
Alice Hsing <alicehsing@gmail.com>
2022-05-11 17:15:31 -0700

    updated initialEntries and initialIndex

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -85,7 +85,8 @@ describe('<App />', () => {
   it('behavioral test - renders detail view', async () => {
     render(
       <MemoryRouter
-        initialEntries={['/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49']}
+        initialEntries={['/', '/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49']}
+        initialIndex={1}
       >
         <App />
       </MemoryRouter>
